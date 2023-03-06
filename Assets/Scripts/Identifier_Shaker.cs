2123de92adb9736b4ee7e307a313d16482acb741
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2023-03-06 19:54:40 +0000

    Collision Testing

---
+++ Assets/Scripts/Identifier_Shaker.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Identifier_Shaker : MonoBehaviour
+{
+
+    public string most_recent_item;
+
+    public LayerMask object_collided;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter (collider collision)
+    {
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ItemIdentity.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemIdentity : MonoBehaviour
+{
+
+    public string my_identity;
+    
+}
