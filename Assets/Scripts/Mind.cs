a97112c513cf38c0609fe8a42812aa6d2904e9e1
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2023-03-22 12:46:06 +0000

    Script

--- Assets/Scripts/Mind.cs
+++ Assets/Scripts/Mind.cs
@@ -7,4 +7,11 @@ public static class Mind
 
     public static bool filling_shaker = false;
 
+    public static int making_stage = 0;
+
+    // 0: Shift started
+    // 1: Recieved order, Placing ingredients
+    // 2: Shaking drink
+    // 3: Serve drink
+
 }
