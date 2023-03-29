67a4a174e2e18a3dfc44b6a95a3a3a3c78c56f50
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2023-03-29 09:35:12 +0100

    Bank 1

---
+++ Assets/Scripts/BankStorage.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BankStorage : MonoBehaviour
+{
+
+    public string[] the_text_to_say;
+    public bool[] is_speaker_a_speaking;
+
+}
