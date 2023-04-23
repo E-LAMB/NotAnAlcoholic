3684b28bb8dd209cb60f25440b888aae3ed80264
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2023-04-23 08:18:23 +0100

    Started main menu

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour
+{
+
+    public GameObject main_menu;
+    public GameObject button_1;
+    public GameObject button_2;
+
+    public float darkness_setting = 1f;
+    public int new_scene;
+
+    public bool going_to_scene;
+    public bool scene_change;
+
+    public Renderer no_ui_menu;
+
+    public void TakeToSceneWithFade(int chosen)
+    {
+        going_to_scene = true;
+        scene_change = true;
+        new_scene = chosen;
+        darkness_setting = 1f;
+        DisableTheMenu();
+    }
+
+    public void ExitApplicationWithFade()
+    {
+        going_to_scene = false;
+        scene_change = true;
+        darkness_setting = 1f;
+        DisableTheMenu();
+    }
+
+    void DisableTheMenu()
+    {
+        main_menu.SetActive(false);
+        button_1.SetActive(false);
+        button_2.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scene_change)
+        {
+            darkness_setting -= Time.deltaTime / 2f;
+            if (darkness_setting < -2f)
+            {
+                if (going_to_scene)
+                {
+                    UnityEngine.SceneManagement.SceneManager.LoadScene(new_scene);
+                } else
+                {
+                    Application.Quit();
+                }
+            }
+        }
+    }
+}
