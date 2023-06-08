28e83c69b56dbaa22dd848ff0884b0f2a697148d
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2023-06-08 19:35:51 +0100

    Bomb Blinks Now

--- Assets/Scripts/ChallengeTimebomb.cs
+++ Assets/Scripts/ChallengeTimebomb.cs
@@ -7,6 +7,13 @@ public class ChallengeTimebomb : MonoBehaviour
 
     public ChallengeBombController my_challenger;
 
+    public bool color;
+    public float timer;
+
+    public SpriteRenderer my_render;
+    public Sprite red;
+    public Sprite blue;
+
     void OnMouseDown()
     {
         my_challenger.bombs -= 1;
@@ -18,5 +25,33 @@ public class ChallengeTimebomb : MonoBehaviour
     {
         my_challenger = GameObject.FindGameObjectWithTag("BombController").GetComponent<ChallengeBombController>();
         my_challenger.bombs += 1;
+        if (Random.Range(0,2) == 1)
+        {
+            color = true;
+        }
+        if (color)
+        {
+            my_render.sprite = blue;
+        } else
+        {
+            my_render.sprite = red;
+        }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 0.4f)
+        {
+            timer -= 0.4f;
+            color = !color;
+            if (color)
+            {
+                my_render.sprite = blue;
+            } else
+            {
+                my_render.sprite = red;
+            }
+        }
     }
 }
